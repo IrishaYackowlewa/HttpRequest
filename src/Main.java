13dcb545d4268ea892fc7d43349f26666f03d55d
IrishaYackowlewa <irisha.yackowlewa.homyak@gmail.com>
2020-06-21 19:51:30 +0300

    modified site content

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,45 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
-        String url = enterURL();
-        readContent(url);
+    public static void main(String[] args) {
+        while(true){
+            try {
+                String url = enterURL();
+                readContent(url);
+                break;
+            } catch (MalformedURLException e) {
+                System.out.println("URL incorrect. Pls write correct URL:");
+            } catch (Exception e) {
+                System.out.println("No access to the specified resource. Pls try again with another resource:");
+            }
+        }
     }
 
     public static void readContent(String url) throws Exception{
-        try {
             URL site = new URL(url);
-            URLConnection URLconnectionStream = site.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            URLconnectionStream.getInputStream()
-                    )
-            );
+            URLConnection UrlConnectionStream = site.openConnection();
+            BufferedReader in = new BufferedReader( new InputStreamReader( UrlConnectionStream.getInputStream()));
 
             String inputLine;
 
             while ((inputLine = in.readLine()) != null)
                 System.out.println(inputLine);
             in.close();
-        } catch (Exception e) {
-            System.out.println("URL incorrect. Pls write correct URL\n");
-            String urlException = enterURL();
-            readContent(urlException);
-        }
     }
 
     public static String enterURL () {
-        System.out.println("Enter yours URL for request\n");
+        System.out.println("Enter yours URL for request:");
         Scanner in = new Scanner(System.in);
         String url = in.nextLine();
 
         return url;
     }
-
+/*
     public static boolean rightURL (String url) {
-        if (url.equals("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")){
+        if (url.matches("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")) {
             return true;
         }
         return false;
     }
+ */
 }
